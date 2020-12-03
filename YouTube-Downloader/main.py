eb61ce0d97852d6370efabd878716198a1a35dc1
Celeste <c.sabater2001@gmail.com>
2020-12-03 13:00:25 +0100

    Ahora los audios se descargan como .mp3

--- YouTube-Downloader/main.py
+++ YouTube-Downloader/main.py
@@ -1,7 +1,16 @@
 import pytube
+import os
+import time
 
 
-def descarga(video, name, option):
+def descarga(video, name, option, path):
+
+    char_nopermitidos = ["/","&","-","_",":","\\"]
+    nombre = ""
+    for letra in name:
+        if letra not in char_nopermitidos:
+            nombre = nombre + letra
+
     if option == 1:
         if 'itag="22"' in str(video.streams):
             stream = video.streams.get_by_itag(22)
@@ -9,8 +18,13 @@ def descarga(video, name, option):
             stream = video.streams.get_highest_resolution()
     else:
         stream = video.streams.filter(only_audio=True)[1]
-    print('Descargando ' + name + '...')
-    stream.download(filename=name)
+
+    print('Descargando ' + nombre + '...')
+    stream.download(filename=nombre)
+    if option == 2:
+        viejo = path + nombre + ".webm"
+        new = path + nombre + ".mp3"
+        os.rename(viejo, new)
     print("¡Listo!")
 
 
@@ -18,8 +32,10 @@ opcion = int(input("Descargar vídeos: 1. Descargar solo audio: 2.\n"))
 while opcion != 1 and opcion != 2:
     opcion = int(input("Opción no válida.\n"))
 opcion2 = int(input("Descargar vídeo sueltos: 1. Descargar playlist: 2.\n"))
-while opcion2 != 1 and opcion != 2:
+while opcion2 != 1 and opcion2 != 2:
     opcion2 = int(input("Opción no válida.\n"))
+
+path = r'D:\github\Proyectos-personales\YouTube-Downloader' + '\\'
 
 if opcion2 == 1:
 
@@ -36,7 +52,7 @@ if opcion2 == 1:
 
     for x in range(len(lista_de_descarga)):
         raw_name = nombres[x]
-        descarga(lista_de_descarga[x], raw_name, opcion)
+        descarga(lista_de_descarga[x], raw_name, opcion, path)
 
 else:
     url = input("Pon la url de la playlist...\n")
@@ -44,4 +60,8 @@ else:
     for url in playlist:
         video = pytube.YouTube(url)
         raw_name = video.title
-        descarga(video, raw_name, opcion)
+        descarga(video, raw_name, opcion, path)
+
+print("")
+print("Gracias por usarme, cerrando automáticamente la venta en 10s...\n")
+time.sleep(10)
